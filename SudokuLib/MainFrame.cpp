ba6de89719ddcb820e790cc04abed3695a279f11
lillianyanke <yankelil@msu.edu>
2023-10-11 17:49:04 -0400

    Changing the name to Action Sudoku

--- SudokuLib/MainFrame.cpp
+++ SudokuLib/MainFrame.cpp
@@ -11,7 +11,7 @@
  */
 void MainFrame::Initialize()
 {
-    Create(nullptr, wxID_ANY, L"Sudoku",
+    Create(nullptr, wxID_ANY, L"Action Sudoku",
            wxDefaultPosition,  wxSize( 1000,800 ));
 
 }
