1837e909a8d4c9521762b7ee8a17d456de129be6
tessjmurphy <tessjmurphy@gmail.com>
2023-10-13 12:51:59 -0400

    sparty class

---
+++ SudokuLib/Sparty.cpp
@@ -0,0 +1,15 @@
+/**
+ * @file Sparty.cpp
+ * @author Tess Murphy
+ */
+
+#include "Sparty.h"
+
+/// Character speed in pixels per second
+const double MaxSpeed = 400.00;
+
+/// The time for an eating cycles in seconds
+const double EatingTime = 0.5;
+
+/// The time for a headbutt cycle in seconds
+const double HeadbuttTime = 0.5;
---
+++ SudokuLib/Sparty.h
@@ -0,0 +1,23 @@
+/**
+ * @file Sparty.h
+ * @author Tess Murphy
+ *
+ *
+ */
+
+#ifndef PROJECT1_SUDOKULIB_SPARTY_H
+#define PROJECT1_SUDOKULIB_SPARTY_H
+
+/**
+ * Class for sparty
+ */
+#include "Item.h"
+class Sparty : public Item
+{
+private:
+
+public:
+
+};
+
+#endif //PROJECT1_SUDOKULIB_SPARTY_H
