809ff6a58aad5edc9ae7986797bf3427d507f624
tessjmurphy <tessjmurphy@gmail.com>
2023-10-15 23:42:22 -0400

    xml load

---
+++ SudokuLib/Background.cpp
@@ -0,0 +1,6 @@
+/**
+ * @file Background.cpp
+ * @author Tess Murphy
+ */
+
+#include "Background.h"
---
+++ SudokuLib/Background.h
@@ -0,0 +1,19 @@
+/**
+ * @file Background.h
+ * @author Tess Murphy
+ *
+ *
+ */
+
+#ifndef PROJECT1_SUDOKULIB_BACKGROUND_H
+#define PROJECT1_SUDOKULIB_BACKGROUND_H
+
+class Background
+{
+private:
+
+public:
+
+};
+
+#endif //PROJECT1_SUDOKULIB_BACKGROUND_H
