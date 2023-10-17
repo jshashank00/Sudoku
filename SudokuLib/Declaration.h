4afae0f9726504ca3fbbbe054159a4a5e4a55692
lillianyanke <yankelil@msu.edu>
2023-10-17 18:46:04 -0400

    declaration class

--- SudokuLib/Declaration.h
+++ SudokuLib/Declaration.h
@@ -7,13 +7,20 @@
 
 #ifndef PROJECT1_SUDOKULIB_DECLARATION_H
 #define PROJECT1_SUDOKULIB_DECLARATION_H
+#include <wx/xml/xml.h>
 
 class Declaration
 {
 private:
+    int mWidth;
+    int mHeight;
+    wxString mImage;
+    int mValue;
 
 public:
+    Declaration();
 
+    void XmlLoad(wxXmlNode *node);
 };
 
 #endif //PROJECT1_SUDOKULIB_DECLARATION_H
