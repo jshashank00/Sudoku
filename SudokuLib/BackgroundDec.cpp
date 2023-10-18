7d1bf21b4ab5c04c3594127291df2d51c8ca83c6
tessjmurphy <tessjmurphy@gmail.com>
2023-10-17 21:05:36 -0400

    declarations

--- SudokuLib/BackgroundDec.cpp
+++ SudokuLib/BackgroundDec.cpp
@@ -1,21 +1,20 @@
 /**
- * @file NumberDec.cpp
+ * @file BackgroundDec.cpp
  * @author Tess Murphy
  */
 
 #include "pch.h"
-#include "NumberDec.h"
+#include "BackgroundDec.h"
 
 /**
  * Load the attributes for a declaration node.
  *
  * This is the  base class version that loads the attributes
- * common to number declarations.
+ * common to background declarations.
  *
  * @param node The Xml node we are loading the dec from
  */
-void NumberDec::XmlLoad(wxXmlNode *node)
+void BackgroundDec::XmlLoad(wxXmlNode *node)
 {
     mImage = node->GetAttribute(L"image",L"0");
-    node->GetAttribute(L"value", L"0").ToInt(&mValue);
 }
---
+++ SudokuLib/BackgroundDec.h
@@ -0,0 +1,21 @@
+/**
+ * @file BackgroundDec.h
+ * @author Tess Murphy
+ *
+ *
+ */
+
+#ifndef PROJECT1_SUDOKULIB_BACKGROUNDDEC_H
+#define PROJECT1_SUDOKULIB_BACKGROUNDDEC_H
+
+#include "Declaration.h"
+class BackgroundDec : public Declaration
+{
+private:
+    wxString mImage;
+public:
+
+    void XmlLoad(wxXmlNode *node);
+};
+
+#endif //PROJECT1_SUDOKULIB_BACKGROUNDDEC_H
