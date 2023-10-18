360a99c844707ed6e33deb5b6711a35656be343c
tessjmurphy <tessjmurphy@gmail.com>
2023-10-17 20:56:51 -0400

    declaration number

--- SudokuLib/NumberDec.cpp
+++ SudokuLib/NumberDec.cpp
@@ -3,4 +3,19 @@
  * @author Tess Murphy
  */
 
+#include "pch.h"
 #include "NumberDec.h"
+
+/**
+ * Load the attributes for an item node.
+ *
+ * This is the  base class version that loads the attributes
+ * common to number declarations.
+ *
+ * @param node The Xml node we are loading the dec from
+ */
+void NumberDec::XmlLoad(wxXmlNode *node)
+{
+    mImage = node->GetAttribute(L"image",L"0");
+    node->GetAttribute(L"value", L"0").ToInt(&mValue);
+}
--- SudokuLib/NumberDec.h
+++ SudokuLib/NumberDec.h
@@ -11,9 +11,11 @@
 class NumberDec : public Declaration
 {
 private:
-
+    wxString mImage;
+    int mValue;
 public:
 
+    void XmlLoad(wxXmlNode *node);
 };
 
 #endif //PROJECT1_SUDOKULIB_NUMBERDEC_H
