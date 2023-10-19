2505259a6c24553e25e35cdbef8c48dc606ef256
tessjmurphy <tessjmurphy@gmail.com>
2023-10-19 12:39:56 -0400

    xray xml load

--- SudokuLib/Xray.cpp
+++ SudokuLib/Xray.cpp
@@ -25,3 +25,24 @@ void Xray::Draw(std::shared_ptr<wxGraphicsContext> graphics, int width, int heig
     // Draw the image at the calculated position
     graphics->DrawBitmap(*mItemBitmap, x, y, wid, hit);
 }
+
+/**
+ * Load the attributes for an item node.
+ *
+ * This is the  base class version that loads the attributes
+ * common to all items. Override this to load custom attributes
+ * for specific items.
+ *
+ * @param node The Xml node we are loading the item from
+ */
+void Xray::XmlLoad(wxXmlNode *itemNode, wxXmlNode *decNode)//, shared_ptr<Declaration> decNode)
+{
+    Item::XmlLoad(itemNode, decNode);
+    wxString image = decNode->GetAttribute(L"image",L"0");
+    image = "images/" + image;
+    Item::SetImage(image);
+    mItemImage = make_unique<wxImage> (image, wxBITMAP_TYPE_ANY);
+    mItemBitmap = make_unique<wxBitmap>(*mItemImage);
+    decNode->GetAttribute(L"capacity", L"0").ToInt(&mCapacity);
+
+}
