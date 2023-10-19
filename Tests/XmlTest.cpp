ebee15157ce519251feba375f199319db81795a3
lillianyanke <yankelil@msu.edu>
2023-10-19 16:22:04 -0400

    level load test

--- Tests/XmlTest.cpp
+++ Tests/XmlTest.cpp
@@ -5,6 +5,7 @@
 
 #include <pch.h>
 #include "gtest/gtest.h"
+#include <fstream>
 
 #include <Sudoku.h>
 #include <LevelLoad.h>
@@ -13,11 +14,28 @@ using namespace std;
 
 class LevelLoadTest : public ::testing::Test {
 
+
 };
 
-TEST_F(LevelLoadTest, Invalid)
-{
+TEST_F(LevelLoadTest, LoadingItems) {
     Sudoku sudoku;
-    wxString filename = "invalid.xml";
-    LevelLoad  level(filename, &sudoku);
+    LevelLoad level("levels/level1.xml", &sudoku);
+    int count = 0;
+    for (std::shared_ptr<Item> item : sudoku.GetItems())
+    {
+        count++;
+    }
+    ASSERT_EQ(count, 82);
+
+    Sudoku sudoku2;
+    LevelLoad level2("levels/level2.xml", &sudoku);
+    int count2 = 0;
+    for (std::shared_ptr<Item> item : sudoku2.GetItems())
+    {
+        count2++;
+    }
+    ASSERT_EQ(count2, 84);
+
 }
+
+
