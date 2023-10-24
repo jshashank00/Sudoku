ca82a1f91ba82454cc85660ffdc8e20ff1a28fd0
tessjmurphy <tessjmurphy@gmail.com>
2023-10-24 19:24:13 -0400

    clear test

--- Tests/SudokuTest.cpp
+++ Tests/SudokuTest.cpp
@@ -7,12 +7,51 @@
 #include <gtest/gtest.h>
 #include <Sudoku.h>
 #include <Item.h>
+#include <Digit.h>
+#include <Given.h>
+
 using namespace std;
 
 class SudokuTest : public::testing::Test {
+protected:
+    /**
+    *  Populate an aquarium with all types
+    */
+    void PopulateDiffTypes(Sudoku *sudoku)
+    {
 
+        auto sparty = make_shared<Sparty>(sudoku);
+        sudoku->Add(sparty);
+        sparty->SetImage("images/sparty-1.png");
+        sparty->SetLocation(100, 200);
+
+        auto digit = make_shared<Digit>(sudoku, "images/0r.png");
+        sudoku->Add(digit);
+        digit->SetLocation(400, 400);
+
+        auto given = make_shared<Given>(sudoku, "images/0b.png");
+        sudoku->Add(given);
+        given->SetLocation(600, 100);
+
+    }
 };
 
 TEST_F(SudokuTest, Construct){
     Sudoku sudoku;
+}
+
+TEST_F(SudokuTest, Clear)
+{
+    // Create an aquarium
+    Sudoku sudoku;
+
+    // Populate aquarium
+    PopulateDiffTypes(&sudoku);
+
+    sudoku.Clear();
+
+    // Assert empty aquarium
+    ASSERT_EQ(sudoku.HitTest(100,200), nullptr);
+    ASSERT_EQ(sudoku.HitTest(400,400), nullptr);
+    ASSERT_EQ(sudoku.HitTest(600,100), nullptr);
 }
