1f72b6b6c6d6d7f2c8a0fc0a29cab3fb88778677
Shashank Jayaram <jayara17@msu.edu>
2023-10-26 18:04:54 -0400

    Digit Visitor (Need ItemVisitor)

---
+++ SudokuLib/DigitVisitor.h
@@ -0,0 +1,39 @@
+/**
+ * @file DigitVisitor.h
+ * @author shash
+ *
+ *
+ */
+
+#ifndef PROJECT1_SUDOKULIB_DIGITVISITOR_H
+#define PROJECT1_SUDOKULIB_DIGITVISITOR_H
+
+#include "Digit.h"
+
+/**
+ * Visits Digits
+ */
+///
+class DigitVisitor
+{
+private:
+bool DigitCheck;
+
+public:
+    DigitVisitor() : DigitCheck(false) {}
+
+//    void VisitDigit(Digit* digit) override
+//    {
+//        DigitCheck = true;
+//    }
+
+    // Add other Visit functions for other item types if needed
+
+    bool DigitChecker() const
+    {
+        return DigitCheck;
+
+
+    };
+
+#endif //PROJECT1_SUDOKULIB_DIGITVISITOR_H
