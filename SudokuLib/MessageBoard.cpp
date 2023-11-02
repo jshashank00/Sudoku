1d2f7c93c185ff94b7b82a58f510a612896c50aa
Shashank Jayaram <jayara17@msu.edu>
2023-11-02 13:16:04 -0400

    Dark Green Level Message, added black border

--- SudokuLib/MessageBoard.cpp
+++ SudokuLib/MessageBoard.cpp
@@ -41,16 +41,19 @@ void MessageBoard::Draw(std::shared_ptr<wxGraphicsContext> graphics, int width, 
     time_t current = time(0);
     time_t elapsed = current - mStartTime;
 
-    if(elapsed <= 3 && !delayElapsed)
+    if (elapsed <= 3 && !delayElapsed)
     {
         mSudoku->SetMessageBoardVisible(true);
         // Set the font and color for the text
-        wxFont font(wxSize(0, 24), wxFONTFAMILY_SWISS, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
-        graphics->SetFont(font, *wxBLACK); // Set the text color to black
 
-        // Draw the white message board
-        graphics->SetBrush(*wxWHITE_BRUSH);
+        // Set the dark green color
+        wxColour darkGreen = wxColour(0, 128, 0);
 
+        // Set the font and color for the text
+        wxFont font(wxSize(0, 24), wxFONTFAMILY_SWISS, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD);
+        graphics->SetFont(font, darkGreen); // Set the text color to dark green
+
+        // Calculate message board dimensions
         int boardWidth = 400;
         int boardHeight = 200;
         int boardX = (width - boardWidth) / 2;
@@ -66,13 +69,15 @@ void MessageBoard::Draw(std::shared_ptr<wxGraphicsContext> graphics, int width, 
             boardWidth = boardWidth + 100;
         }
 
+        // Draw the black border
+        graphics->SetPen(wxPen(*wxBLACK, 5));
+        graphics->SetBrush(*wxWHITE_BRUSH);
         graphics->DrawRectangle(boardX, boardY, boardWidth, boardHeight);
 
-        // Draw "Level 1" message in green
-        font = wxFont(wxSize(60, 60), wxFONTFAMILY_SWISS, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL);
-        graphics->SetFont(font, *wxGREEN); // Set the text color to green
+        // Draw "Level 1" message in dark green and bold
+        font = wxFont(wxSize(60, 60), wxFONTFAMILY_SWISS, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD);
+        graphics->SetFont(font, darkGreen);
 
-        //wxString levelMessage = "Level 1";
         double levelTextWidth, levelTextHeight;
         graphics->GetTextExtent(levelMessage, &levelTextWidth, &levelTextHeight);
         graphics->DrawText(levelMessage, boardX + (boardWidth - levelTextWidth) / 2, boardY);
@@ -83,7 +88,7 @@ void MessageBoard::Draw(std::shared_ptr<wxGraphicsContext> graphics, int width, 
 
         boardY += levelTextHeight; // Move below "Level 1"
 
-        for(const wxString &msg : messages)
+        for (const wxString &msg : messages)
         {
             graphics->GetTextExtent(msg, &levelTextWidth, &levelTextHeight);
             graphics->DrawText(msg, boardX + (boardWidth - levelTextWidth) / 2, boardY);
@@ -99,9 +104,10 @@ void MessageBoard::Draw(std::shared_ptr<wxGraphicsContext> graphics, int width, 
     {
         delayElapsed = true;
         mStartTime = current;
-
     }
 }
+
+
     // Check if showMessage flag is set
 //    if (mXray->ShouldShowMessage())
 //    {
